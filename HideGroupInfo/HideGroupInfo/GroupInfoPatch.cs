ee23f1ae74f34c243b02b00e070a67f27a7a897d
Siim Raudsepp <siimraudsepp@msn.com>
2024-12-19 14:04:07 +0200

    Initial release

---
+++ HideGroupInfo/HideGroupInfo/GroupInfoPatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.Server;
+
+namespace HideGroupInfo;
+
+public class GroupInfoPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ServerySystemPlayerGroups), "CmdgroupInfo")]
+    public static bool Prefix(ref TextCommandResult __result, TextCommandCallingArgs args)
+    {
+        if (args.Caller.Player.Role.Code == "admin" || args.Caller.Player.Groups.Any(group => group.GroupName == args[0] as string))
+        {
+            return true;
+        }
+        
+        __result = TextCommandResult.Error("This command is only available for members of this group and admins");
+        return false;
+    }
+}
---
+++ HideGroupInfo/HideGroupInfo/HideGroupInfoModSystem.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Server;
+using Vintagestory.API.Common;
+using Vintagestory.Server;
+
+namespace HideGroupInfo;
+
+public class HideGroupInfoModSystem : ModSystem
+{
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        var harmony = new Harmony(Mod.Info.ModID);
+
+        var original = AccessTools.Method(typeof(ServerySystemPlayerGroups), "CmdgroupInfo");
+        var patch = AccessTools.Method(typeof(GroupInfoPatch), nameof(GroupInfoPatch.Prefix));
+
+        harmony.Patch(original, new HarmonyMethod(patch));
+    }
+
+}
